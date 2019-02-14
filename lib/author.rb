90ef1c4541bc072f4f7646ee59d1a005858719c7
Dylan Michael Labat <dml252@live.com>
2019-02-14 02:06:53 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,5 @@
 class Author
-  attr_accessor :name
+  attr_accessor :name, :posts
   
   @@post_count = 0
   
@@ -13,13 +13,13 @@ class Author
   end
   
   def add_post(post)
-    self.posts << [post]
+    self.posts << post
     post.author = self
     @@post_count += 1
   end
   
-  def add_post_by_title(name)
-    post = Post.new(name)
+  def add_post_by_title(title)
+    post = Post.new(title)
     self.posts << post
     post.author = self
     @@post_count += 1
